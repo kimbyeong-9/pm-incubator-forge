6d1fde6bd77b7dc867962953f7c0c27c82204dff
haedeune <164822314+haedeune@users.noreply.github.com>
2025-12-31 16:12:41 +0900

    feat: [이해든] 배열 자르기(slice)[PMIF-x]

--- haedeun/javascript/javascript-app/src/App.tsx
+++ haedeun/javascript/javascript-app/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { ArraySliceExample } from './array/ArraySliceExample'
 import { ControlFlowFor } from './control_flow/ControlFlowFor'
 import { ControlFlowForSumExample } from './control_flow/ControlFlowForSumExample'
 import { ControlFlowForSummation } from './control_flow/ControlFlowForSummation'
@@ -7,6 +8,7 @@ import { ControlFlowSwitch } from './control_flow/ControlFlowSwitch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataTypes'
 import { LogicalOperationExample } from './logical_operation/LogicalOperationExample'
 import { MapExample } from './map/MapExample'
+import { MapFilterExample } from './map/MapFilterExample'
 import { MapReduceExample } from './map/MapReduceExample'
 import { CalculationExample } from './math_operation/CalculationExample'
 import { FirstProblem } from './problem/FirstProblem'
@@ -34,7 +36,9 @@ function App() {
         <ControlFlowForSumExample/>
         <SecondProblem/>
         <MapExample/>
-        <MapReduceExample/>  
+        <MapReduceExample/>
+       <MapFilterExample/>   
+       <ArraySliceExample/>
       </div>
 
     </>
